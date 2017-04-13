30167df6dd458d2bb855f38db27378de80f69f50
Igor Mykhaylyuk <igor13luk@gmail.com>
2017-04-13 23:17:05 +0300

    initial commit

---
+++ test-example/test.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect;
+// var assert = require('assert');
+describe('String#split', function(){
+    it('should return an array', function(){
+  //      assert(Array.isArray('a,b,c'.split(',')));
+      // assert.equal(true, true);
+        expect(true).to.be.true;
+    });
+})
